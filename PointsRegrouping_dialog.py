4d8ba4e0875382c6764ff09ce60710b7e79e5adf
Maks <samael500@gmail.com>
2016-07-23 12:26:02 +0300

    :repeat: plugin template use

--- PointsRegrouping_dialog.py
+++ PointsRegrouping_dialog.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- PointsRegrouping
+ PointsRegroupingDialog
                                  A QGIS plugin
  Copies point within the polygon predetermined number of times.
                              -------------------
         begin                : 2016-07-23
+        git sha              : $Format:%H$
         copyright            : (C) 2016 by Maks Skorokhod / WB-Tech
         email                : samael500@gmail.com
-        git sha              : $Format:%H$
  ***************************************************************************/
 
 /***************************************************************************
@@ -19,17 +19,23 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
 """
 
+import os
 
-# noinspection PyPep8Naming
-def classFactory(iface):  # pylint: disable=invalid-name
-    """Load PointsRegrouping class from file PointsRegrouping.
+from PyQt4 import QtGui, uic
 
-    :param iface: A QGIS interface instance.
-    :type iface: QgsInterface
-    """
-    #
-    from .PointsRegrouping import PointsRegrouping
-    return PointsRegrouping(iface)
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'PointsRegrouping_dialog_base.ui'))
+
+
+class PointsRegroupingDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(PointsRegroupingDialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
