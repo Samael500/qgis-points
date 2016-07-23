adf927fae70e191f91ca825195c8bb7dc701ec07
Maks <samael500@gmail.com>
2016-07-23 14:34:55 +0300

    :repeat: upd plugin selecting

--- PointsRegrouping.py
+++ PointsRegrouping.py
@@ -20,17 +20,25 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+from qgis.gui import *
+from qgis.core import *
+
+from PointsRegroupingModule import PointsRegroupingProcessor
+
 # Initialize Qt resources from file resources.py
 import resources
-# Import the code for the dialog
-from PointsRegrouping_dialog import PointsRegroupingDialog
+
 import os.path
 
 
 class PointsRegrouping:
     """QGIS Plugin Implementation."""
+
+    _name = 'PointsRegrouping'
 
     def __init__(self, iface):
         """Constructor.
@@ -58,135 +66,90 @@ class PointsRegrouping:
             if qVersion() > '4.3.3':
                 QCoreApplication.installTranslator(self.translator)
 
-        # Create the dialog (after translation) and keep reference
-        self.dlg = PointsRegroupingDialog()
+        self.toolbar = self.iface.addToolBar(self._name)
+        self.toolbar.setObjectName(self._name)
 
-        # Declare instance attributes
-        self.actions = []
-        self.menu = self.tr(u'&PointsRegroupingPlugin')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'PointsRegrouping')
-        self.toolbar.setObjectName(u'PointsRegrouping')
+    def createToolButton(self, parent, text):
+        button = QToolButton(parent)
+        button.setObjectName(text)
+        button.setToolButtonStyle(Qt.ToolButtonIconOnly)
+        button.setPopupMode(QToolButton.MenuButtonPopup)
+        parent.addWidget(button)
+        return button
 
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
+    def createAction(self, icon_path, text, callback):
+        action = QAction(
+            QIcon(icon_path),
+            text,
+            self.iface.mainWindow())
+        # connect the action to the run method
+        action.setCheckable(True)
+        action.toggled.connect(callback)
+        return action
 
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('PointsRegrouping', message)
-
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
+    def createClearAction(self, icon_path, text):
+        action = QAction(
+            QIcon(icon_path),
+            text,
+            self.iface.mainWindow())
+        # connect the action to the run method
+        action.setCheckable(False)
+        action.triggered.connect(self.clear)
         return action
 
     def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
+        # # Create action that will start plugin configuration
+        # self.actionCriar = self.createAction(
+        #     ":/plugins/PointsRegrouping/icon.png",
+        #     u"Multi Selection by point",
+        #     self.run)
 
-        icon_path = ':/plugins/PointsRegrouping/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'Points Regrouping Plugin'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
+        # Create action that will start plugin configuration
+        self.actionRectangle = self.createAction(
+            ":/plugins/PointsRegrouping/icon.png",
+            u"Select by Rectangle",
+            self.run)
 
+        # Create action that will start plugin configuration
+        self.actionClear = self.createClearAction(
+            ":/plugins/PointsRegrouping/icon.png",
+            u"Clear selections")
+
+        # self.tool = MultiLayerSelection(self.iface.mapCanvas(), self.actionCriar)
+        self.toolRectangle = PointsRegroupingProcessor(self.iface.mapCanvas(), self.actionRectangle)
+
+        #QToolButtons
+        self.selectionButton = self.createToolButton(self.toolbar, u'PointsRegroupingButton')
+        # self.selectionButton.addAction(self.actionCriar)
+        self.selectionButton.addAction(self.actionRectangle)
+        self.selectionButton.addAction(self.actionClear)
+        # set default active
+        self.selectionButton.setDefaultAction(self.actionRectangle)
 
     def unload(self):
-        """Removes the plugin menu item and icon from QGIS GUI."""
-        for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&PointsRegroupingPlugin'),
-                action)
-            self.iface.removeToolBarIcon(action)
-        # remove the toolbar
-        del self.toolbar
+        # Remove the plugin menu item and icon
+        self.iface.mainWindow().removeToolBar(self.toolbar)
 
+    def clear(self):
+        # self.actionCriar.setChecked(False)
+        self.actionRectangle.setChecked(False)
+        self.selectionButton.setDefaultAction(self.selectionButton.sender())
+        layers = self.iface.mapCanvas().layers()
+        for layer in layers:
+            layer.removeSelection()
 
-    def run(self):
-        """Run method that performs all the real work"""
-        # show the dialog
-        self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+    # def run(self, b):
+    #     self.actionCriarRectangle.setChecked(False)
+    #     self.selectionButton.setDefaultAction(self.selectionButton.sender())
+    #     if b:
+    #         self.iface.mapCanvas().setMapTool(self.tool)
+    #     else:
+    #         self.iface.mapCanvas().unsetMapTool(self.tool)
+
+    def run(self, b):
+        # self.actionCriar.setChecked(False)
+        self.selectionButton.setDefaultAction(self.selectionButton.sender())
+        if b:
+            self.iface.mapCanvas().setMapTool(self.toolRectangle)
+        else:
+            self.iface.mapCanvas().unsetMapTool(self.toolRectangle)
